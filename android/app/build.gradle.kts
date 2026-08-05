5fed426e034f796adcb8f30f6c009aaa9c3f5c27
BpkSamed <abrahamsuhartanto1990@gmail.com>
2026-08-05 13:45:46 +0700

    commit 29

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.example.aplikasi_ordo_karmel_inter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
